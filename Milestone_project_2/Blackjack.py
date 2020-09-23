0ae2a6730c77fe7233264ba086f4c8541b3159ae
gianno97 <fabio.giannoccoli@gmail.com>
2020-09-24 00:06:29 +0200

    Update Blackjack.py

--- Milestone_project_2/Blackjack.py
+++ Milestone_project_2/Blackjack.py
@@ -32,9 +32,11 @@ class Deck:
 
     def initial_cards(self):
         self.four_cards = []
-        
-        for i in range(4):
+        self.counter = 0
+
+        while(self.counter < 4):
             self.four_cards.append(self.all_cards.pop(0))
+            self.counter += 1
 
         return self.four_cards
 
@@ -46,29 +48,36 @@ class Cash:
     def __init__(self, amount):
         self.amount = amount
 
+    def add(self, bet):
+        self.amount += bet
+    
+    def subtract(self, bet):
+        self.amount -= bet
+
 
 game_on = True
 
 print("Gioco del Blackjack testuale:")
 
 while(game_on):
-    # print("1.Gioca\n2.Esci")
-    # fsjkajdfkajfkd
     x = input("1.Gioca\n2.Esci\n")
 
     if(x == "1"):
-        new_player = Player(input("Inserisci nome giocatore: "))
         new_amount = Cash(100)
-        print(new_player.name + " hai a disposizione", new_amount.amount)
-        bet = int(input("Fai una puntata "))
+        print("Il giocatore ha a disposizione", new_amount.amount)
+        bet = input("Fai una puntata ")
 
         while(True):
-            if(bet > 0 and bet <= new_amount.amount):
-                print("OK!")
-                break
-            else:
+            if(int(bet) != True):
                 print("Valore non valido!")
-                bet = int(input("Fai una puntata "))
+            else:
+                bet = int(bet)
+                if(bet > 0 and bet <= new_amount.amount):
+                    print("OK!")
+                    break
+                else:
+                    print("Valore non valido!")
+                    bet = input("Fai una puntata ")
         
         new_deck = Deck()
         new_deck.shuffle()
@@ -94,7 +103,8 @@ while(game_on):
                     total_player += card_player.value
                 
                 if(total_player == 21):
-                    print("Blackjack! Il giocatore ha vinto")
+                    print("Blackjack! Il giocatore ha vinto", bet)
+                    new_amount.add(bet)
                     break
                 elif(total_player < 21):
                     print("Carte del giocatore:\n")
@@ -102,7 +112,8 @@ while(game_on):
                         print(card_player)
                     continue
                 else:
-                    print("La somma è", total_player, "Il giocatore ha perso")
+                    print("La somma è", total_player, "Il giocatore ha perso", bet)
+                    new_amount.subtract(bet)
                     break
             elif(action == "2"):
                 while(True):
@@ -117,15 +128,18 @@ while(game_on):
 
                     if(total_dealer == 21):
                         print("Blackjack! Il dealer ha vinto")
+                        new_amount.subtract(bet)
                         break
                     elif(total_dealer <= total_player):
                         dealer_cards.append(new_deck.hit())
                     else:
                         if(total_dealer <= 21):
                             print("Il dealer ha vinto")
+                            new_amount.subtract(bet)
                             break
                         else:
-                            print("Il giocatore ha vinto")
+                            print("Il giocatore ha vinto", bet)
+                            new_amount.add(bet)
                             break
             else:
                 print("Valore non valido")
